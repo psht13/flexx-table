c2f6d5d25a54ccb68febb89c274d8b0fce68012a
Pavlo Yurchenko <pashtet.gm@gmail.com>
2024-11-27 16:13:18 +0200

    update: add api route for flexx-table

--- src/app/api/pages/flexx-table/route.ts
+++ src/app/api/pages/flexx-table/route.ts
@@ -5,9 +5,13 @@ import { NextResponse } from 'next/server'
 import { data } from '@/app/api/fake-db/pages/flexx-table'
 
 export async function GET(req: Request) {
-  const total = req
+  const url = new URL(req.url)
+  const page = Number(url.searchParams.get('page') ?? '1')
+  const limit = Number(url.searchParams.get('limit') ?? '10')
 
-  console.log(total)
+  const startIndex = (page - 1) * limit
+  const endIndex = startIndex + limit
+  const partialData = data.slice(startIndex, endIndex)
 
-  return NextResponse.json(data)
+  return NextResponse.json({ data: partialData, total: data.length })
 }
