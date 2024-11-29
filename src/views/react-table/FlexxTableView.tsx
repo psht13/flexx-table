5b9c7e2d325c3a2b7800fde5c4ae2821fffa68ad
Pavlo Yurchenko <pashtet.gm@gmail.com>
2024-11-29 18:36:12 +0200

    update: add cards

--- src/views/react-table/FlexxTableView.tsx
+++ src/views/react-table/FlexxTableView.tsx
@@ -6,7 +6,6 @@ import { useEffect, useState } from 'react'
 
 // MUI Imports
 import Card from '@mui/material/Card'
-import CardHeader from '@mui/material/CardHeader'
 
 // Third-party Imports
 import { createColumnHelper, flexRender, getCoreRowModel, useReactTable } from '@tanstack/react-table'
@@ -17,7 +16,7 @@ import { Checkbox, TablePagination } from '@mui/material'
 import styles from '@core/styles/table.module.css'
 
 // Type Imports
-import type { FlexxTableCardsDataType, FlexxTableType } from '@/types/pages/flexxTableType'
+import type { FlexxTableType } from '@/types/pages/flexxTableType'
 
 // Column Definitions
 const columnHelper = createColumnHelper<FlexxTableType>()
@@ -54,8 +53,11 @@ const columns = [
     header: 'Policyholder'
   }),
   columnHelper.accessor('amount', {
-    cell: info => info.getValue(),
-    header: 'Amount'
+    cell: info =>
+      new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+      }).format(info.getValue())
   }),
   columnHelper.accessor('method', {
     cell: info => info.getValue(),
@@ -64,7 +66,7 @@ const columns = [
   columnHelper.accessor('status', {
     cell: info => (
       <span
-        className='rounded-full bg-blue-300 text-white block text-center w-26 py-0.5'
+        className='rounded-full bg-purple-400 text-white font-bold block text-center w-26 py-0.5'
         dangerouslySetInnerHTML={{
           __html: info.getValue()
         }}
@@ -86,7 +88,7 @@ const columns = [
     id: 'action',
     header: 'Action',
     cell: () => (
-      <div className='flex gap-8'>
+      <div className='flex justify-center gap-4'>
         <div className='ri-eye-line cursor-pointer'></div>
         <div className='ri-file-edit-line cursor-pointer'></div>
       </div>
@@ -97,7 +99,7 @@ const columns = [
 const FlexxTableView = () => {
   // States
   const [data, setData] = useState<[FlexxTableType] | []>([])
-  const [cardsData, setCardsData] = useState<FlexxTableCardsDataType | null>(null)
+  const [cardsData, setCardsData] = useState(null)
 
   const [perPage, setPerPage] = useState(10)
   const [page, setPage] = useState(0)
@@ -156,12 +158,38 @@ const FlexxTableView = () => {
   }
 
   // Conditions
-  if (isLoading) return <div className='text-center text-lg'>Loading...</div>
+  if (isLoading) return <div className='text-center h-full text-lg'>Loading...</div>
 
   return (
     <Card>
-      <CardHeader title='Flexx Table' />
       <div className='overflow-x-auto'>
+        {cardsData && (
+          <div className='flex items-center justify-between gap-12'>
+            {Object.keys(cardsData).map((el, i) => (
+              <div className='flex flex-col gap-6 rounded-xl p-8 shadow-md' key={el}>
+                <div className='flex items-start justify-between gap-6 h-16 w-[220px]'>
+                  <span className='capitalize text-lg w-12'>
+                    {el.length > 8 ? el.slice(0, 7) + ' ' + el.slice(7) : el}
+                  </span>
+                  <span className='rounded-xl p-2 bg-purple-400 flex items-center justify-center'>
+                    <span className='ri-account-circle-line w-8 h-8 text-white' />
+                  </span>
+                </div>
+                <div className='text-3xl font-bold'>
+                  {i + 1 !== Object.keys(cardsData).length
+                    ? new Intl.NumberFormat('en-US', {
+                        style: 'currency',
+                        currency: 'USD'
+                      }).format(cardsData[el])
+                    : new Intl.NumberFormat('en-US', {
+                        currency: 'USD'
+                      }).format(cardsData[el])}
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+
         <table className={styles.table}>
           <thead>
             {table.getHeaderGroups().map(headerGroup => (
@@ -189,6 +217,7 @@ const FlexxTableView = () => {
               ))}
           </tbody>
         </table>
+
         <TablePagination
           component='span'
           count={total}
